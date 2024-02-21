fe6eacfaa97b5c56733be5b67f51bc4a89006035
Trigail Curtis <trigailcurtisabm@gmail.com>
2024-02-21 17:16:02 -0600

    init

---
+++ MethodOverloading24/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading24;
+internal class Methods
+{
+    public static  int Add(int x, int y)
+    { 
+        return x + y;
+    }
+
+    public static double Add(double x, double y)
+    { 
+        return x + y;
+    }
+        
+    public static decimal Add(decimal x, decimal y)
+    { 
+        return x + y; 
+    }
+
+    public static string Add(int x, int y, bool withDollars)
+    {
+        if (withDollars && (x +y) !=1)
+        {
+            return $"{x + y} dollars";
+        }
+        else if (withDollars && (x +y) == 1)
+        {
+            return $"{x + y} dolllar";
+        }
+        else 
+        {
+            return $"{x + y}";
+        }
+    }
+
+
+}
+
---
+++ MethodOverloading24/Program.cs
@@ -0,0 +1,5 @@
+﻿using MethodOverloading24;
+
+var answer = Methods.Add(2,2, true);
+Console.WriteLine(answer);
+
